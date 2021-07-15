9c5556c8732fbac639858e4f26ec165279c24df9
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2021-07-15 18:32:31 -0300

    Style update/Bug fix

--- gFinances/view/finances.py
+++ gFinances/view/finances.py
@@ -1,11 +1,11 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
 
 from control.process import Process
 
 class Finances(Screen):
     def __init__(self, **kwargs):
         super(Finances, self).__init__(**kwargs)
-
         self.process = Process()
 
     def on_enter(self, *args, **kwargs):
@@ -13,6 +13,6 @@ class Finances(Screen):
         expense = self.process.RequestExpense()
         total = float(receivement) - float(expense)
 
-        self.ids.lblReceivement.text = receivement
-        self.ids.lblExpense.text = expense
-        self.ids.lblTotal.text = str(total)
+        self.ids.lblReceivement.text = "R$ " + receivement
+        self.ids.lblExpense.text = "R$ " + expense
+        self.ids.lblTotal.text = "R$ " + str(total)
